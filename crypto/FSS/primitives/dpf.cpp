d4411420141b09a89d9ce056820c9f021730aa9c
Applied Data Security Lab <admin@adslab.cn>
2025-11-10 16:11:55 +0800

    Update dpf.cpp

--- crypto/FSS/primitives/dpf.cpp
+++ crypto/FSS/primitives/dpf.cpp
@@ -523,3 +523,182 @@ std::pair<GroupElement, GroupElement> evalAll_reduce_et(int party, DPFETKeyPack 
     evalAll_reduce_helper_et(party, key, rightShift, tab, out, corr, s, t, 0, 0);
     return std::make_pair(out, corr);
 }
+
+
+std::pair<DPFETKeyPack, DPFETKeyPack> keyGenDPFGT(int bin, GroupElement idx)
+{
+    always_assert(bin <= 64);
+    always_assert(bin >= 8);
+    static const block notOneBlock = toBlock(~0, ~1);
+    const static block pt[2] = {ZeroBlock, OneBlock};
+
+    DPFETKeyPack key0(bin);
+    DPFETKeyPack key1(bin);
+
+    int tid = omp_get_thread_num();
+    auto s = FSSConfig::prngs[tid].get<std::array<block, 2>>();
+    auto s0 = s[0];
+    auto s1 = s[1];
+
+    s0 = s0 & notOneBlock;
+    s1 = s1 & notOneBlock;
+    key0.s[0] = s0;
+    key1.s[0] = s1;
+    
+    u8 t0 = 0;
+    u8 t1 = 1;
+
+    block ct0[2];
+    block ct1[2];
+
+    for (int i = 0; i < bin - 7; ++i)
+    {
+        const u8 keep = static_cast<uint8_t>(idx >> (bin - 1 - i)) & 1;
+        const u8 loose = keep ^ 1;
+
+        AES ak0(s0);
+        AES ak1(s1);
+
+        ak0.ecbEncTwoBlocks(pt, ct0);
+        ak1.ecbEncTwoBlocks(pt, ct1);
+
+        auto scw = (ct0[loose] ^ ct1[loose]) & notOneBlock;
+        u64 tcw[2];
+        u64 &tLcw = tcw[0];
+        u64 &tRcw = tcw[1];
+        tLcw = lsb(ct0[0]) ^ lsb(ct1[0]) ^ keep ^ 1;
+        tRcw = lsb(ct0[1]) ^ lsb(ct1[1]) ^ keep;
+
+        key0.s[i+1] = scw;
+        key1.s[i+1] = scw;
+        key0.tLcw |= (tLcw << (bin - 1 - i));
+        key0.tRcw |= (tRcw << (bin - 1 - i));
+
+        if (t0 == 0)
+        {
+            s0 = ct0[keep] & notOneBlock;
+            t0 = lsb(ct0[keep]);
+        }
+        else
+        {
+            s0 = (ct0[keep] & notOneBlock) ^ scw;
+            t0 = lsb(ct0[keep]) ^ tcw[keep];
+        }
+
+        if (t1 == 0)
+        {
+            s1 = ct1[keep] & notOneBlock;
+            t1 = lsb(ct1[keep]);
+        }
+        else
+        {
+            s1 = (ct1[keep] & notOneBlock) ^ scw;
+            t1 = lsb(ct1[keep]) ^ tcw[keep];
+        }
+    }
+
+    key1.tLcw = key0.tLcw;
+    key1.tRcw = key0.tRcw;
+
+    if (t0 == 1) s0 = s0 ^ OneBlock;
+    if (t1 == 1) s1 = s1 ^ OneBlock;
+    uint64_t e0, e1;
+    GroupElement ip = idx % 128;
+    if (ip >= 64) {
+        e0 = 0;
+        e1 = 1ULL << (127 - ip);
+    }
+    else {
+        e0 = 1ULL << (63 - ip);
+        e1 = 0;
+    }
+    key0.leaf = s0 ^ s1 ^ osuCrypto::toBlock(e0, e1);
+    key1.leaf = key0.leaf;
+
+    return std::make_pair(key0, key1);
+}
+
+
+GroupElement evalDPFGT_GT(int party, const DPFETKeyPack &key, GroupElement x)
+{
+    static const block notOneBlock = toBlock(~0, ~1);
+    int bin = key.bin;
+
+    auto s = _mm_loadu_si128(key.s);
+    u8 t = party;
+    
+    u8 x_prev = 1;
+    u8 t_dcf = 0;
+
+    for (int i = 0; i < bin - 7; ++i)
+    {
+        assert(lsb(s) == 0);
+        const u8 x_i = static_cast<uint8_t>(x >> (bin - 1 - i)) & 1;
+
+        if (x_prev != x_i)
+        {
+            t_dcf = t_dcf ^ t;
+        }
+        x_prev = x_i;
+
+        AES ak(s);
+        block ct = ak.ecbEncBlock(toBlock(0, x_i));
+        s = ct & notOneBlock;
+        u8 t_old = t;
+        t = lsb(ct);
+
+        if (t_old) {
+            s = s ^ _mm_loadu_si128(key.s + i + 1);
+            t = t ^ ((key.tcw[x_i] >> (bin - 1 - i)) & 1);
+        }
+    }
+
+    osuCrypto::block leaf = s;
+    if (t) leaf = leaf ^ OneBlock ^ key.leaf;
+    uint64_t b;
+
+    {
+        const u8 x_i = static_cast<uint8_t>(x >> 6) & 1;
+        if (x_prev != x_i)
+        {
+            t_dcf = t_dcf ^ t;
+        }
+        x_prev = x_i;
+        if (x_i) 
+        {
+            b = _mm_extract_epi64(leaf, 0);
+        }
+        else 
+        {
+            b = _mm_extract_epi64(leaf, 1);
+        }
+        t = __builtin_parityll(b);
+    }
+
+    GroupElement xp = x % 64;
+
+    if (x_prev == 0)
+    {
+        for (int i = 0; i <= xp; ++i)
+        {
+            t_dcf = t_dcf ^ ((b >> (63 - i)) & 1);
+        }
+    }
+    else
+    {
+        for (int i = xp + 1; i < 64; ++i)
+        {
+            t_dcf = t_dcf ^ ((b >> (63 - i)) & 1);
+        }
+    }
+    if (party==0)
+    {
+       t_dcf = t_dcf ^ 0;
+    }
+    else{
+        t_dcf = t_dcf ^ 1;
+    }
+    
+
+    return t_dcf;
+}
